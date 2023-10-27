c1558256747413ecd20872ae64a0cbcb2a30e38a
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 08:35:42 -0300

    cp2

---
+++ animais.js
@@ -0,0 +1,7 @@
+const animais = ['leão', 'tigre', 'macaco', 'rinoceronte', 'karpa'];
+
+if (animais.includes('leão')){
+    console.log('leão existe na lista animais.');
+} else {
+    console.log('leão não existe na lista animais.');
+}
---
+++ filmes.js
@@ -0,0 +1,5 @@
+const filmes = ['Ben Hur', 'Poderoso Chefão', 'Paulo de Tarso'];
+
+if (filmes.length == 3){
+    console.log("possui três itens.")   
+}
